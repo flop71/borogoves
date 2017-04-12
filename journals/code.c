9b26a7deb1a0939254023e2f180f157cc0573fe8
David Malone <dwmalone@Davids-MBP-2.hamilton.local>
2017-04-12 14:24:36 +0100

    Should only be white space changes to tidy up indenting and formatting.

--- journals/code.c
+++ journals/code.c
@@ -4,7 +4,6 @@
 #include <math.h>
 #include <sys/time.h>
 
-
 double lognormal_service(void);
 double pareto_arrival(void);
 
@@ -18,34 +17,33 @@ double exp_arrival(void);
 double exp_service(void);
 
 int main(void) {
-int len, arrivals;
-double next_arrival, next_departure, now;
+	int len, arrivals;
+	double next_arrival, next_departure, now;
 
+	len = 0;
+	now = 0;
+	next_arrival = now + pareto_arrival();
+	arrivals = 1;
+	next_departure = -1;
 
-len = 0;
-now = 0;
-next_arrival = now + pareto_arrival();
-arrivals = 1;
-next_departure = -1;
-
-printf("time\tlen\n");
+	printf("time\tlen\n");
 	
-/* Simulates 1000 outage event arrivals */
+	/* Simulates 1000 outage event arrivals */
 
 	while (arrivals < 1000) { 
-	if (next_departure < 0 || next_arrival < next_departure) {
-		arrivals++;
-		len++;
-		now = next_arrival;
-		next_departure = now + lognormal_service();
-		next_arrival = now + pareto_arrival();
-	} else {
-		len--;
-		now = next_departure;
-		if (len > 0) {
+		if (next_departure < 0 || next_arrival < next_departure) {
+			arrivals++;
+			len++;
+			now = next_arrival;
 			next_departure = now + lognormal_service();
+			next_arrival = now + pareto_arrival();
 		} else {
-			next_departure = -1;
+			len--;
+			now = next_departure;
+			if (len > 0) {
+				next_departure = now + lognormal_service();
+			} else {
+				next_departure = -1;
 			}
 		}
 		printf("%f\t%d\n", now, len);
@@ -55,7 +53,6 @@ printf("time\tlen\n");
 }
 
 double pareto_arrival(void) {
-
 	float a;
 	float b;
 	float inv_fun_denom;
@@ -68,35 +65,27 @@ double pareto_arrival(void) {
 	par_arr = (b/inv_fun_denom)-b;
 
 	return par_arr;
-
 }
 
 
 double lognormal_service (void) {
-
-    float m;    //Mean parameter
-    float s;    //stdev parameter
-    float x1, x2, w, y1;
-    static float y2;
-    static int use_last = 0;
-    float normal_result;
-    float lnormal_result;
-
-
-    // dummy mean log and sd log
-    m =  4;
-    s =  1.1;
+	float m;	//Mean parameter
+	float s;	//stdev parameter
+	float x1, x2, w, y1;
+	static float y2;
+	static int use_last = 0;
+	float normal_result;
+	float lnormal_result;
 
 
-   if (use_last)  /* use value from previous call */
-	{
+	// dummy mean log and sd log
+	m =  4;
+	s =  1.1;
+
+	if (use_last) {  /* use value from previous call */
 		y1 = y2;
 		use_last = 0;
-	}
-	
-	else
-	
-	{
+	} else {
 		do {
 			x1 = 2.0 * (rand() / (RAND_MAX + 1.0)) - 1.0;
 			x2 = 2.0 * (rand() / (RAND_MAX + 1.0)) - 1.0;
@@ -110,9 +99,8 @@ double lognormal_service (void) {
 		use_last = 1;
 	}
 
-
 	normal_result = (m + y1 * s); // produces normal distribution output
 	lnormal_result = exp(normal_result);
 
-        return lnormal_result;
+	return lnormal_result;
 }
